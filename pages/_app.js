e1deef158693d12f60d876b873517c21f5a53c93
Dipansh <depanshsaxena.ds@gmail.com>
2022-03-08 05:54:43 +0530

    chakra ui added into the project

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,23 @@ import App from "next/app";
 import { Provider } from "react-redux";
 import { createWrapper } from "next-redux-wrapper";
 import { store } from "../redux";
+import { ChakraProvider } from '@chakra-ui/react'
 import React from "react";
+
 import "../styles/globals.css";
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 
 class MyApp extends App {
   static async GetInitialProps({ Component, ctx }) {
@@ -17,7 +32,9 @@ class MyApp extends App {
     const { Component, appProps } = this.props;
     return (
       <Provider store={store}>
+      <ChakraProvider theme={theme}>
         <Component {...appProps} />
+      </ChakraProvider>
       </Provider>
     );
   }
